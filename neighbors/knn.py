f68f5d45445eb356b0c716aa06e884929f0ec9b8
Visakh Padmanabhan <visakhpadmanabhan7@gmail.com>
2022-06-15 22:07:23 +0200

    Updated KNN wiht py

--- neighbors/knn.py
+++ neighbors/knn.py
@@ -1,7 +1,14 @@
 from sklearn.neighbors import NearestNeighbors
+import pandas as pd
 
 #return the data frame with nearest neighbors of the element
 def knn(df, data_point, n_neighbors):
     neigh = NearestNeighbors(n_neighbors=n_neighbors)
     neigh.fit(df)
     return neigh.kneighbors(data_point)
+dataset=pd.read_csv("../feature_extraction/Word2Vec.csv",index_col="docno")
+Docid = ' LA122989-0001 '
+neighbours=50
+query_point=dataset[dataset.index==Docid]
+print(query_point)
+print(knn(dataset, query_point, neighbours))
