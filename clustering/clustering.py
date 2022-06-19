1e118a56e6d55789fd0df546bd66781fa3e66c72
Harshit Juneja <harshit@Harshits-MacBook-Air.local>
2022-06-19 12:45:16 +0200

    update clustering logic

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,18 +1,7 @@
 import math
 import numpy as np
 from scipy.spatial import distance
-from sklearn.utils import shuffle
 
-#K-means
-# assign initial centres
-# assigns clusters to -1
-# calculates cluster assignment
-# prev_clusters = initial clusters
-# while (calculated_clusters = prev_clusters)
-#   calculate_new_centroids
-#   calculate_eucledian_dist
-#   penalize constraints
-#   new cluster assignment
 def k_means(num_clusters, df, init_centres, pos_docs, neg_docs, neu_docs, must_link_penalty, cannot_link_penalty):
 
     #returns a list of k initial centre points for cluster initialization
@@ -22,57 +11,60 @@ def k_means(num_clusters, df, init_centres, pos_docs, neg_docs, neu_docs, must_l
         df_centres = df[::math.ceil(every_x_item)]
         return df_centres.iloc[:num_clusters]
 
-    #one time initialization steps    
-    #add new columns in dataframe that would contain distance values from the point to the centre
-    # df1 = pd.DataFrame(columns=list(map(lambda x: "dist_c"+str(x), range(num_clusters))))
-    # df = df.join(df1, how="outer")
-    # print(df)
-
     #choose centre points from data if not already given
     if(len(init_centres)!=num_clusters):
-        num_centres = num_clusters/3
+        centres = []
+        num_centres = math.floor(num_clusters/3)
         rem_centres = num_clusters%3
         pos_centers = choose_initial_centres(num_centres+rem_centres, pos_docs)
         neg_centers = choose_initial_centres(num_centres, neg_docs)
         neu_centres = choose_initial_centres(num_centres, neu_docs)
-        centres = centres.append(pos_centers, neg_centers, neu_centres)
-        print(centres)
-        
+        centres = np.concatenate((pos_centers, neg_centers, neu_centres))
+    
     #determine distance of points from the centres and assign clusters
     # df = shuffle(df)
     data = df.to_numpy()
     col_count = len(data[:][0])
-    centroids = centres.to_numpy()
+    centroids = centres[:,:col_count-1]
     data = np.hstack([data, np.zeros((len(data),1)), np.ones((len(data),1))])
     #compute the centroids till the cluster assignment remains the same
-    fit(data, col_count, centroids, num_clusters, must_link_penalty, cannot_link_penalty)
+    fit(data, df, col_count, pos_docs, neg_docs, neu_docs, centroids, num_clusters, must_link_penalty, cannot_link_penalty)
+
+def extract_sentiment_index(df, pos_docs, neg_docs,  neu_docs):
+    pos_docs_loc = df.index.get_indexer(pos_docs.index.to_list())
+    neg_docs_loc = df.index.get_indexer(neg_docs.index.to_list())
+    neu_docs_loc = df.index.get_indexer(neu_docs.index.to_list())
+    return pos_docs_loc, neg_docs_loc, neu_docs_loc
 
 #takes in a dataframe and a center vector and outputs a series with distance values of all points from the vector
 #last 2 columns reserved for cluster comparision (current cluster and prev cluster)
-def fit(data, col_count, centroids, num_clusters, must_link_penalty, cannot_link_penalty):
+def fit(data, df, col_count, pos_docs, neg_docs, neu_docs, centroids, num_clusters, must_link_penalty, cannot_link_penalty):
     iter = 0
-    pos_docs_loc, neg_docs_loc, neu_docs_loc = extract_sentiment_index()
+    pos_docs_loc, neg_docs_loc, neu_docs_loc = extract_sentiment_index(df, pos_docs, neg_docs, neu_docs)
+    # print(data[:,col_count+1])
+    # print(col_count)
+    # print(data[:,:col_count+1])
+    
     while(not np.array_equiv(data[:,col_count+1],data[:,col_count])):
         if(iter!=0):
             data[:,col_count] = data[:,col_count+1]
             data[:,col_count+1] = -1
-            centroids = update_centroids(num_clusters, data[:,:col_count],col_count)
+            centroids = update_centroids(num_clusters, data[:,:col_count+1],col_count)
         iter+=1
-        dist = []
-        for point in data:
+        for row_index, point in enumerate(data):
             dist_val = []
-            for index, center in centroids:
+            for index, center in enumerate(centroids):
                 eucledian_dist = distance.euclidean(point[:col_count-1], center)
-                penalty_dist = penalize(point, col_count, index, pos_docs_loc, neg_docs_loc, must_link_penalty, cannot_link_penalty)
+                penalty_dist = penalize(point, data, col_count, index, pos_docs_loc, neg_docs_loc, must_link_penalty, cannot_link_penalty)
                 dist_val.append(eucledian_dist+penalty_dist)
             cluster_val = dist_val.index(min(dist_val))
-            dist.append(cluster_val)
-        data[:,col_count+1] = dist
-    print('iterations--->',iter)
-
+            data[row_index,col_count+1] = cluster_val
+        print('iteration count--->',iter)
+    return data
+    # pd.DataFrame(data).to_csv('./sentiment_clusters.csv')
 
 # penalize point for not being assigned to must link peers and being assigned to cannot link peers:
-def penalize(point, col_count, assumed_pt_cluster, pos_docs_loc, neg_docs_loc, must_link_penalty, cannot_link_penalty):
+def penalize(point, data, col_count, assumed_pt_cluster, pos_docs_loc, neg_docs_loc, must_link_penalty, cannot_link_penalty):
     
     penalty = 0.0
 
@@ -90,32 +82,27 @@ def penalize(point, col_count, assumed_pt_cluster, pos_docs_loc, neg_docs_loc, m
     
     #return negative penalty for neutral sentiment documents
     for ml_pt in must_link_set:
-        if np.not_equal(data[ml_pt],point) and data[ml_pt][col_count+1] != -1 and assumed_pt_cluster != data[ml_pt][col_count+1]:
+        if np.any(np.not_equal(data[ml_pt][:col_count-1],point[:col_count-1])) and data[ml_pt][col_count+1] != -1 and assumed_pt_cluster == data[ml_pt][col_count+1]:
+            print("here for ml penalty")
             penalty += must_link_penalty
 
     #return positive penalty for  sentiment documents
     for cl_pt in cannot_link_set:
-        if np.not_equal(data[cl_pt],point) and data[cl_pt][col_count+1] != -1 and assumed_pt_cluster == data[cl_pt][col_count+1]:
+        if np.any(np.not_equal(data[cl_pt][:col_count-1],point[:col_count-1])) and data[cl_pt][col_count+1] != -1 and assumed_pt_cluster == data[cl_pt][col_count+1]:
+            print("here for cl penalty")
             penalty += cannot_link_penalty
 
     return penalty
 
 #handles case where no point is assigned to a cluster center
 def update_centroids(num_clusters, data, col_count):
-    #num_columns = len(df.shape[1])
-    #make this dynamic: take as many columns as are there in the dataframe
-    # clusters_with_no_pts = df[:4].nunique() - num_clusters
-    #choose random samples as cluster centres
-    # if (clusters_with_no_pts>0):
-    #     centers = df.sample(n=clusters_with_no_pts)
-    #find mean by cluster value and call eucledian distance again
     
     centroids = []
     data_per_cluster = []
     for clus_no in range(num_clusters):
         data_per_cluster.append(data[data[:,col_count]== float(clus_no)])
     
-    for cluster_data in range(len(data_per_cluster)):
-        centroids.append(np.mean(cluster_data[:,0:col_count-1], axis=0))
+    for cluster_data in data_per_cluster:
+        centroids.append(np.mean(cluster_data[:,:col_count-1], axis=0))
     
     return centroids
